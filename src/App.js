2da84bc1f96b083f5c02e836513800a38a826c2a
Ilya Anatskiy <devvvyoff@gmail.com>
2020-11-06 14:08:56 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
+import axios from 'axios';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Chat from './components/Chat';
+
+import JoinForm from './components/JoinForm';
+import reducer from './reducer';
+import socket from './socket';
 
 function App() {
+  const [state, dispatch] = React.useReducer(reducer, {
+    userName: null,
+    roomId: null,
+    joined: false,
+    users: [],
+    messages: [],
+  });
+
+  const onLogin = async (obj) => {
+    dispatch({
+      type: 'JOINED',
+      payload: obj,
+    });
+    socket.emit('ROOM:JOIN', obj);
+    const { data } = await axios.get(`/rooms/${obj.roomId}`);
+    console.log(data);
+    dispatch({
+      type: 'SET_DATA',
+      payload: data,
+    });
+  };
+
+  const setUsers = (users) => {
+    dispatch({
+      type: 'SET_USERS',
+      payload: users,
+    });
+  };
+
+  const addMessage = (message) => {
+    console.log(message);
+    dispatch({
+      type: 'NEW_MESSAGE',
+      payload: message,
+    });
+  };
+
+  React.useEffect(() => {
+    socket.on('ROOM:SET:USERS', setUsers);
+    socket.on('ROOM:NEW_MESSAGE', addMessage);
+  }, []);
+  console.log(state);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      {!state.joined ? <JoinForm onLogin={onLogin} /> : <Chat {...state} addMessage={addMessage} />}
     </div>
   );
 }
